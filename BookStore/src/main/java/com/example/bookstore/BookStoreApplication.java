e421c7d8fa4cc100924b6b7ce83f442f4f9f0255
MaSG <masg@MaSGs-MacBook-Pro.local>
2020-09-18 16:47:30 +0300

    Formatting

--- BookStore/src/main/java/com/example/bookstore/BookStoreApplication.java
+++ BookStore/src/main/java/com/example/bookstore/BookStoreApplication.java
@@ -28,12 +28,9 @@ public class BookStoreApplication {
 
 			log.info("Few test book");
 			
-			repository.save(new Book("Sapiens: A Brief History of Humankind", "	Yuval Noah Harari", 2011,
-					"978-952-279-470-3", 12.00));
-			repository.save(new Book("Homo Deus: A Brief History of Tomorrow", "	Yuval Noah Harari", 2016,
-					"978-191-070-187-4", 15.00));
-			repository.save(new Book("Zen and the Art of Motorcycle Maintenance", "Robert M. Pirsig", 1974,
-					"0-688-00230-7", 17.00));
+			repository.save(new Book("Sapiens: A Brief History of Humankind", "Yuval Noah Harari", 2011, "978-952-279-470-3", 12.00));
+			repository.save(new Book("Homo Deus: A Brief History of Tomorrow", "Yuval Noah Harari", 2016, "978-191-070-187-4", 15.00));
+			repository.save(new Book("Zen and the Art of Motorcycle Maintenance", "Robert M. Pirsig", 1974, "0-688-00230-7", 17.00));
 
 			log.info("Showing your books");
 			
