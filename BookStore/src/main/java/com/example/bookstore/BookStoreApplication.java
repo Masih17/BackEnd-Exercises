061062fbf8d4bd25ec1823bbd65e28a5ca5aee29
MaSG <masg@MaSGs-MacBook-Pro.local>
2020-09-18 16:42:51 +0300

    BookstoreCrud done

--- BookStore/src/main/java/com/example/bookstore/BookStoreApplication.java
+++ BookStore/src/main/java/com/example/bookstore/BookStoreApplication.java
@@ -1,13 +1,46 @@
 package com.example.bookstore;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.example.bookstore.domain.Book;
+import com.example.bookstore.domain.BookRepository;
 
 @SpringBootApplication
 public class BookStoreApplication {
+
+	private static final Logger log = LoggerFactory.getLogger(BookStoreApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookStoreApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner bookListCommand(BookRepository repository) {
+		return (args) -> {
+
+			// Your code...add some demo data to db
+
+			log.info("Few test book");
+			
+			repository.save(new Book("Sapiens: A Brief History of Humankind", "	Yuval Noah Harari", 2011,
+					"978-952-279-470-3", 12.00));
+			repository.save(new Book("Homo Deus: A Brief History of Tomorrow", "	Yuval Noah Harari", 2016,
+					"978-191-070-187-4", 15.00));
+			repository.save(new Book("Zen and the Art of Motorcycle Maintenance", "Robert M. Pirsig", 1974,
+					"0-688-00230-7", 17.00));
+
+			log.info("Showing your books");
+			
+			for (Book book : repository.findAll()) {
+				log.info(book.toString());
+			}
+
+		};
+	}
 }
--- BookStore/src/main/java/com/example/bookstore/web/BookController.java
+++ BookStore/src/main/java/com/example/bookstore/web/BookController.java
@@ -1,16 +1,49 @@
 package com.example.bookstore.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import java.util.Optional;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+
+
 import com.example.bookstore.domain.Book;
+import com.example.bookstore.domain.BookRepository;
+
 
 @Controller
 public class BookController {
+	
+	@Autowired
+	private BookRepository repository;
 
-	@RequestMapping(value = "/home", method = RequestMethod.GET)
-	public String home(@RequestParam (value="book") Book book)  { 
-	return "home";
+	@RequestMapping(value = "/list", method = RequestMethod.GET)
+	public String bookList(Model model) {
+		model.addAttribute("books", repository.findAll());
+		return "booklist";
 	}
+	
+	@RequestMapping(value = "/add")
+    public String addBook(Model model){
+    	model.addAttribute("book", new Book());
+        return "addbook";
+    }  
+    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    public String save(Book book){
+        repository.save(book);
+        return "redirect:list";
+    }  
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+    public String deleteBook(@PathVariable("id") Long bookId, Model model) {
+    	repository.deleteById(bookId);
+        return "redirect:../list"; //double dot because the delete must 
+    }    
+    @RequestMapping(value = "/modify/{id}", method = RequestMethod.GET)
+    public String modifyBook(@PathVariable("id") Long bookId, Model model) {
+    	Optional<Book> book = repository.findById(bookId); // optional means there might be a book with the ID or not
+    	model.addAttribute("book", book);
+        return "modifybook";
+    }   
 }
